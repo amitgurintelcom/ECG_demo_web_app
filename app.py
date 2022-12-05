d3a73aae35c51155d1c541bae2d891574fbc6bba
gal.sabina.star <gal.sabina.star@intel.com>
2022-12-05 13:55:52 +0200

    show ECG image as expected (convert npy to image)

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from st_aggrid.shared import JsCode
 import numpy as np
 from PIL import Image
 from scipy import signal
+import ecg_plot
 ###################################
 is_exception_raised = False
 output = None
@@ -83,47 +84,19 @@ def select_host(selected):
         # conn_req="/api/v1/endpoints/cukczelw3sytfuga7byy"
     return api_key, conn_addr, conn_req
 
-
-def signal_preprocess(ecg_npy):
-    # load ecg numpy file and remove extra dimension
-    ecg_npy = np.squeeze(ecg_npy, axis=0)
-
-    # upsample if necessary
-    if ecg_npy.shape[0] == 2500:
-        ecg_npy = signal.resample(ecg_npy, 5000)
-
-    # try normalizing to local min/max as npy contains negative values
-    a_min = np.min(ecg_npy)
-    a_max = np.max(ecg_npy)
-    ecg_npy = ((ecg_npy - a_min) * (1 / (a_max - a_min)) * 255).astype('uint8')
-
-    combo_npy = np.zeros(50176).reshape(224, 224)
-
-    #for i in range(0, 22):
-    for i in range(0, 18): # need to ignore first frames
-        from_pixels=i*224
-        to_pixels=(i+1)*224
-        slice_npy = ecg_npy[from_pixels:to_pixels, :]
-
-        from_dest = i*12
-        to_dest = (i + 1)*12
-        combo_npy[:, from_dest:to_dest] = slice_npy
-
-    ecg_img = Image.fromarray(combo_npy)  # convert numpy array to image
-    ecg_img = ecg_img.convert(mode="RGB")
-
-    return ecg_img
-
 def present_ecg_image(content):
     date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S")
-    temp_name = "temp_" + date + ".npy"
-    with open(temp_name, "wb") as file_o:
+    temp_name = "temp_" + date
+    with open(temp_name + ".npy", "wb") as file_o:
         numpy_img = file_o.write(base64.b64decode(content))
     
-    numpy_img = np.load(temp_name, allow_pickle=True)
+    numpy_img = np.load(temp_name + ".npy", allow_pickle=True)[0].transpose() / 1000
 
-    image = signal_preprocess(numpy_img)
-    st.image(image, width=200, caption='Uploaded ECG image')
+    ecg_plot.plot(numpy_img, sample_rate = 500, title = 'ECG 12')
+    ecg_plot.save_as_png(temp_name,'./')
+    st.image(temp_name + '.png', caption='Uploaded ECG image')
+    os.remove(temp_name + ".npy")
+
     
 st.set_page_config(page_icon="✂️", page_title="ECG Prediction", layout="wide")
 
@@ -171,7 +144,6 @@ if uploaded_file is not None:
     with st.spinner('This might take few seconds ... '):
         try:
             conn = http.client.HTTPSConnection(conn_addr)
-            st.info('Sending File to the server')
             conn.request("POST", conn_req, payload, headers)
             st.info('Got server POST response')
             res = conn.getresponse()
@@ -191,15 +163,16 @@ if not is_exception_raised and output is not None:
     cardiac_ejection=re.sub(r'.*fraction\"\,(0.\d{3}).*',r'\1', output)
     cardiac_ejection_perc=float(cardiac_ejection)*100
 
-    c1, c2, c3 = st.columns([5,4,4])
+    c1, c2 = st.columns([5,4])
     with c1:
-        present_ecg_image(encoded_string)
-    with c2:
         st.metric(label="Non-normal cardiac ejection fraction probability", value=f"{mortality_chance_perc}%")
         st.metric(label=f'Cardiac ejection fraction', value=f"{cardiac_ejection_perc}%")
-    with c3:
+    with c2:
         st.metric(label=f'Gender', value=f"{gender}")
         st.metric(label=f'Gender Confidence', value=f"{prob_perc}%")
+    present_ecg_image(encoded_string)
+    
+        
 else:
     st.info(f"""👆 Please upload a .npy ECG file first""")
     st.stop()
